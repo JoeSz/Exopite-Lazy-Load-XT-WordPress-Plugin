e35b4a1c57035d26dce4bacab36bcaf35e629b1d
Joe <ujbeszel@gmail.com>
2019-03-01 17:44:29 +0100

    Add function to properly detect REST API calls and update update url

--- exopite-lazy-load-xt/includes/class-exopite-lazy-load-xt.php
+++ exopite-lazy-load-xt/includes/class-exopite-lazy-load-xt.php
@@ -161,6 +161,32 @@ class Exopite_Lazy_Load_Xt {
 
 	}
 
+    /**
+     * Checks if the current request is a WP REST API request.
+     *
+     * Case #1: After WP_REST_Request initialisation
+     * Case #2: Support "plain" permalink settings
+     * Case #3: URL Path begins with wp-json/ (your REST prefix)
+     *          Also supports WP installations in subfolders
+     *
+     * @returns boolean
+     * @author matzeeable
+     * @link https://wordpress.stackexchange.com/questions/221202/does-something-like-is-rest-exist/317041#317041
+     * @link https://gist.github.com/matzeeable/dfd82239f48c2fedef25141e48c8dc30
+     */
+    public function is_rest() {
+        $prefix = rest_get_url_prefix( );
+        if (defined('REST_REQUEST') && REST_REQUEST // (#1)
+            || isset($_GET['rest_route']) // (#2)
+                && strpos( trim( $_GET['rest_route'], '\\/' ), $prefix , 0 ) === 0)
+            return true;
+
+        // (#3)
+        $rest_url = wp_parse_url( site_url( $prefix ) );
+        $current_url = wp_parse_url( add_query_arg( array( ) ) );
+        return strpos( $current_url['path'], $rest_url['path'], 0 ) === 0;
+    }
+
 	/**
 	 * Register all of the hooks related to the public-facing functionality
 	 * of the plugin.
@@ -169,6 +195,13 @@ class Exopite_Lazy_Load_Xt {
 	 * @access   private
 	 */
 	private function define_public_hooks() {
+
+		if ( ( defined( 'JSON_REQUEST' ) && JSON_REQUEST ) ||
+			 $this->is_rest() ||
+			 ( defined('XMLRPC_REQUEST') && XMLRPC_REQUEST ) ||
+			 ( defined('DOING_AJAX') && DOING_AJAX ) ||
+			 is_admin()
+		) return;
 
 		$plugin_public = new Exopite_Lazy_Load_Xt_Public( $this->get_plugin_name(), $this->get_version() );
 
